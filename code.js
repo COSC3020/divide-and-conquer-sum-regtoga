a7de967572738bee39c3972bbe7455126776588b
Aidan Newberry <91752566+regtoga@users.noreply.github.com>
2025-01-30 23:32:04 -0700

    just a small timer commit

--- code.js
+++ code.js
@@ -22,10 +22,101 @@ function divideAndConquerSum(a) {
     return divideAndConquerSum(a.slice(0, midpointleft)) + divideAndConquerSum(a.slice(midpointleft, midpointright)) + divideAndConquerSum(a.slice(midpointright));
 }
 
+
+// great timer class, can start() stop() reset() and getTime()
+class Timer {
+    constructor () {
+      this.isRunning = false;
+      this.startTime = 0;
+      this.overallTime = 0;
+    }
+  
+    _getTimeElapsedSinceLastStart () {
+      if (!this.startTime) {
+        return 0;
+      }
+    
+      return Date.now() - this.startTime;
+    }
+  
+    start () {
+      if (this.isRunning) {
+        return console.error('Timer is already running');
+      }
+  
+      this.isRunning = true;
+  
+      this.startTime = Date.now();
+    }
+  
+    stop () {
+      if (!this.isRunning) {
+        return console.error('Timer is already stopped');
+      }
+  
+      this.isRunning = false;
+  
+      this.overallTime = this.overallTime + this._getTimeElapsedSinceLastStart();
+    }
+  
+    reset () {
+      this.overallTime = 0;
+  
+      if (this.isRunning) {
+        this.startTime = Date.now();
+        return;
+      }
+  
+      this.startTime = 0;
+    }
+  
+    getTime () {
+      if (!this.startTime) {
+        return 0;
+      }
+  
+      if (this.isRunning) {
+        return this.overallTime + this._getTimeElapsedSinceLastStart();
+      }
+  
+      return this.overallTime;
+    }
+  }
+
+
+var random = [];
+for (var i = 0; i < 100000000; i++){
+    random.push(Math.floor(Math.random() * 10));
+}
+
+function iterativesum(b) {
+    var sum = 0;
+    for (var i = 0; i < b.length; i++){
+        sum += b[i];
+    }
+    return sum;
+}
+
+const timer = new Timer();
+  timer.start();
+  setInterval(() => {
+    const timeInSeconds = Math.round(timer.getTime() / 1000);
+    document.getElementById('time').innerText = timeInSeconds;
+  }, 100)
+
+console.log(iterativesum(random));
+console.log("time = " + timer.getTime());
+
+timer.reset();
+console.log(divideAndConquerSum(random));
+console.log("time = " + timer.getTime());
+timer.stop();
+
+
 //some of my test cases
 //console.log(divideAndConquerSum([]) + "\n"); //0
 //console.log(divideAndConquerSum([0, 1, 2]) + "\n"); // 3
-//console.log(divideAndConquerSum([0, 1, 2, 3]) + "\n"); //6
+//console.log(divideAndConquerSum([1, 5, 5]) + "\n"); //6
 //console.log(divideAndConquerSum([0, 1, -2, 3]) + "\n"); //2
 //console.log(divideAndConquerSum([1, 2, 3, 9, 10 ,11]) + "\n"); //36
 //console.log(divideAndConquerSum([1, 2, 3, 9,-10, 10, 11]) + "\n"); //26
